53d88e7ab03ad2a7a3aafa28fa24828d9d13acf7
Henry <henrykc24@gmail.com>
2022-05-04 17:50:09 +0100

    Add rule and final implementation

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -19,58 +19,58 @@ const TodoList = ({
   return (
     <div className="parent_container">
       <Droppable droppableId="todosList">
-        {(provided) => (
-          <React.StrictMode>
-            <div
-              className="todos_container active_tasks"
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-            >
-              <span className="todo_heading">Active Tasks</span>
-              {todos
-                .map((todo, index) => (
-                  <SingleTodo
-                    type="not-done"
-                    index={index}
-                    key={`${todo.id}`}
-                    todo={todo}
-                    todos={todos}
-                    setTodos={setTodos}
-                    completedTodos={completedTodos}
-                    setCompletedTodos={setCompletedTodos}
-                  />
-                ))}
-              {provided.placeholder}
-            </div>
-          </React.StrictMode>
+        {(provided, snapshot) => (
+          <div
+            className={`todos_container active_tasks ${
+              snapshot.isDraggingOver ? "todo_dragged" : ""
+            }`}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="todo_heading">Active Tasks</span>
+            <hr className="hr" />
+            {todos.map((todo, index) => (
+              <SingleTodo
+                type="not-done"
+                index={index}
+                key={`${todo.id}`}
+                todo={todo}
+                todos={todos}
+                setTodos={setTodos}
+                completedTodos={completedTodos}
+                setCompletedTodos={setCompletedTodos}
+              />
+            ))}
+            {provided.placeholder}
+          </div>
         )}
       </Droppable>
 
       <Droppable droppableId="todosCompleted">
-        {(provided) => (
-          <React.StrictMode>
-            <div
-              className="todos_container completed_tasks"
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-            >
-              <span className="todo_heading">Completed Tasks</span>
-              {completedTodos
-                .map((todo, index) => (
-                  <SingleTodo
-                    type="done"
-                    index={index}
-                    key={`${todo.id}`}
-                    todo={todo}
-                    todos={todos}
-                    setTodos={setTodos}
-                    completedTodos={completedTodos}
-                    setCompletedTodos={setCompletedTodos}
-                  />
-                ))}
-              {provided.placeholder}
-            </div>
-          </React.StrictMode>
+        {(provided, snapshot) => (
+          <div
+            className={`todos_container completed_tasks ${
+              snapshot.isDraggingOver ? "completed_dragged" : ""
+            }`}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="todo_heading">Completed Tasks</span>
+            <hr className="hr" />
+            {completedTodos.map((todo, index) => (
+              <SingleTodo
+                type="done"
+                index={index}
+                key={`${todo.id}`}
+                todo={todo}
+                todos={todos}
+                setTodos={setTodos}
+                completedTodos={completedTodos}
+                setCompletedTodos={setCompletedTodos}
+              />
+            ))}
+            {provided.placeholder}
+          </div>
         )}
       </Droppable>
     </div>
